0d3f848425691a3638542545590daec4388f7e46
HobnobMancer <emma.e.hobbs@outlook.com>
2024-04-22 09:46:10 +0100

    parser now retrieves sites data
    also added docstrings and renamed some variables

--- scripts/hmmer/parser_domtbl.py
+++ scripts/hmmer/parser_domtbl.py
@@ -4,46 +4,86 @@ import sys
 COMMENT_LINE = "#"
 
 
-def parse(hmmer_domtbl: str):
+def parse(hmmer_domtbl: str, retrieve_sites: bool):
+    """Parse hmmer output into a JSON object.
+
+    :param hmmer_domtbl: str repr of path to hmmer.dtbl file
+    :param retrieve_sites: bool, if to retrieve site annotations
+        [only true for SFLD and CDD.]
+        [Site annotation is added to the hmmer output files by
+        in house post-processing scripts.]
+    """
     sequence_matches = {}
     with open(hmmer_domtbl, "r") as dtbl_f:
         current_seq = None
-        acc = []
+        models = []
         domains = []
         for line in dtbl_f.readlines():
             if not line.startswith(COMMENT_LINE):
                 info = line.split()
+
                 if info[0] != current_seq:
                     if current_seq:
-                        sequence_matches[current_seq] = acc
-                    acc = []
+                        sequence_matches[current_seq] = models
+                    models = []
                     current_seq = info[0]
                 if info[9] == info[10]:
-                    domains.append(get_domain(info))
-                    acc.append(get_accession(info, domains))
+                    domains.append(get_domain_data(info))
+                    models.append(get_full_seq_data(info, domains))
                     domains = []
                 else:
-                    domains.append(get_domain(info))
+                    domains.append(get_domain_data(info))
 
-            if current_seq:
-                sequence_matches[current_seq] = acc
+                if line.startswith("[I6-SITES]"):
+                    if retrieve_sites:
+                        continue
+                    else:
+                        break
+
+                if line.startswith("[site]") and retrieve_sites:
+                    # add last domain hit to be processe
+                    sequence_matches[current_seq] = models
+                    # then process the sites
+                    current_seq = info[1]
+                    for _site in get_site(info):
+                        try:
+                            sequence_matches[current_seq]["sites"].append(_site)
+                        except KeyError:
+                            sequence_matches[current_seq]["sites"] = [_site]
+
+            if current_seq and retrieve_sites is False:
+                sequence_matches[current_seq] = models
+
     return sequence_matches
 
 
-def get_accession(info, domains):
-    acc_info = {
+def get_full_seq_data(info: list[str], domains: dict[str, str]) -> dict[str, str]:
+    """
+    Retrieve data for the full sequence hit against the model:
+        These are the data listed under --- full sequence ---
+
+    :param info: list, line split by blankspace
+    :param domains: list of dicts, one dict per domain hit
+    """
+    model_info = {
         "query_name": info[3],
-        "accession": info[4],
+        "modelaccession": info[4],
         "qlen": int(info[5]),
         "e_value": float(info[6]),
         "score": float(info[7]),
         "bias": float(info[8]),
-        "domains": domains
+        "domains": domains,
     }
-    return acc_info
+    return model_info
 
 
-def get_domain(info):
+def get_domain_data(info: list[str]) -> dict[str, str]:
+    """
+    Retrieve the data for the specific domain hit:
+        These are the data listed under --- this domain ---
+
+    :param info: list, line split by blankspace
+    """
     domain_info = {
         "cEvalue": info[11],
         "iEvalue": info[12],
@@ -55,15 +95,34 @@ def get_domain(info):
         "ali_to": info[18],
         "env_from": info[19],
         "env_to": info[20],
-        "accession": info[21],
+        "modelsession": info[21],
         "description_of_target": info[22]
     }
     return domain_info
 
 
+def get_site(info: list[str]):
+    """
+    Retrieve data for site hits. SFLD and CDD only.
+
+    :param info: list, line split by blankspace
+    """
+    for residue in info[3].split(","):
+        yield {
+            "model_ac": info[2],
+            "residue": residue,
+            "description": " ".join(info[4:])
+        }
+
+
 def main():
+    """
+    :args 0: str repr of path to hmmer file to be parsed
+    :args 1: bool whether to retrieve site annotations
+        from file [true for SFLD and CDD]
+    """
     args = sys.argv[1:]
-    parse_result = parse(args[0])
+    parse_result = parse(args[0], args[1])
     print(json.dumps(parse_result, indent=2))
 
 
