6ac22039dbea15a63833c7da9ab32c62617a6485
HobnobMancer <emma.e.hobbs@outlook.com>
2024-05-28 00:34:42 +0100

    add filtering ips6 json gene3d hits

---
+++ scripts/members/gene3d_funfam/filter_ips6_hits.py
@@ -0,0 +1,167 @@
+import argparse
+import json
+import sys
+
+from pathlib import Path
+
+
+class Gene3dHit:
+    def __init__(self):
+        self.sequence_id = None
+        self.domains = {}
+
+    def add_domain(self, value: str):
+        domain_id = value.split()[3]
+        domain = DomainHit()
+        domain.domain_id = domain_id
+        domain.cath_superfamily = value.split()[1]
+        domain.score = value.split()[4]
+        domain.evalue = value.split()[-1]
+        domain.boundaries_start = value.split()[-5].split("-")[0]
+        domain.boundaries_end = value.split()[-5].split("-")[1]
+        domain.resolved = value.split()[-4]
+        domain.aligned_regions = value.split()[-3]
+
+        if domain_id not in self.domains:
+            self.domains[domain_id] = [domain]
+        else:
+            self.domains[domain_id].append(domain)
+
+
+class DomainHit:
+    def __init__(self):
+        self.domain_id = None
+        self.cath_superfamily = None
+        self.score = None  # bit score
+        self.evalue = None  # indp-evalue
+        self.boundaries_start = None  # envelope boundaries
+        self.boundaries_end = None
+        self.resolved = None
+        self.aligned_regions = None
+
+
+def build_parser() -> argparse.ArgumentParser:
+    """Build cmd-line argument parser"""
+    parser = argparse.ArgumentParser(
+        prog="gene3d_and_funfam_match_parser",
+        description="Parse the output from the add_cath_superfamilies.py into the interal IPS6 JSON structure",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "ips6",
+        type=Path,
+        help="Path to an internal IPS6 JSON file"
+    )
+
+    parser.add_argument(
+        "cath_out",
+        type=Path,
+        help="Path to cath_out file"
+    )
+
+    return parser
+
+
+def parse_cath(cath_out: Path) -> dict[str, Gene3dHit]:
+    """Parse cath_out file into a dictionary
+
+    :param cath_out: Path to add_cath_superfamilies.py output file
+    """
+    matches = {}  # prot seq id: domains
+
+    with open(cath_out, "r") as fh:
+        for line in fh.readlines():
+            if line.startswith('#'):
+                continue  # header row
+            protein_id = line.split()[2]
+            if protein_id not in matches:
+                match = Gene3dHit()
+                match.sequence_id = protein_id
+                matches[protein_id] = match
+            matches[protein_id].add_domain(line)
+
+    return matches
+
+
+def filter_matches(ips6: Path, gene3d_matches: dict[str, Gene3dHit]) -> dict:
+    """Parse the IPS6 JSON file, filtering hits to only retains
+    those that passed the Gene3D post-processing.
+
+    :param ips6: path to internal IPS6 JSON file containing parsed hits from HMMER.out file
+    :param gene3d_matches: dict of Gene3dHits, representing hits in the 
+        add_cath_superfamilies.py output file
+    """
+    processed_ips6 = {}
+    with open(ips6, "r") as fh:
+        ips6_data = json.load(fh)
+
+    for protein_id in ips6_data:
+        if protein_id not in gene3d_matches:
+            continue
+
+        for signature_acc in ips6_data[protein_id]:
+            if signature_acc not in gene3d_matches[protein_id].domains:
+                continue
+
+            # retrieve the relevant domain hit
+            gene3d_location = None  # from the IPS6 data
+            gene3d_domain = None  # from the cath-superfamilies output
+            for location in ips6_data[protein_id][signature_acc]["locations"]:
+                for domain in gene3d_matches[protein_id].domains[signature_acc]:
+                    if domain.evalue == location["evalue"] \
+                        and domain.score == location["score"] \
+                        and domain.boundaries_start == location["envelopeStart"] \
+                        and domain.boundaries_end == location["envelopeEnd"]:
+                        gene3d_location = location
+                        gene3d_domain = domain
+
+                if not gene3d_location:
+                    continue
+
+                # found a relevant location in the IPS6 data and cath-superfams data
+
+                if protein_id not in processed_ips6:
+                    processed_ips6[protein_id] = {}
+
+                cath_superfam = gene3d_domain.cath_superfamily
+                gene3d_sig_acc = f"G3DSA:{cath_superfam}"
+                if gene3d_sig_acc not in processed_ips6[protein_id]:
+                    # signature_acc is the domain id
+                    # replace the domain id with the Cath superfamily
+                    sig_info = ips6_data[protein_id][signature_acc]
+                    sig_info["accession"] = f"G3DSA:{cath_superfam}"
+
+                    # model ac is the domain id (minus the -... suffix)
+                    sig_info["model-ac"] = signature_acc.split("-")[0]
+
+                    processed_ips6[protein_id][gene3d_sig_acc] = sig_info
+                    processed_ips6[protein_id][gene3d_sig_acc]["locations"] = []
+                    # start locations as empty as not all hits/locations in ips6 
+                    # may have parsed the post-processing
+
+                # add the location fragments (the 'aligned-regions') to the domain location data
+                gene3d_location["location-fragments"] = []
+                for fragment in gene3d_domain.aligned_regions.split(","):
+                    gene3d_location["location-fragments"].append({
+                        "start": fragment.split("-")[0],
+                        "end": fragment.split("-")[1],
+                        "dc-status": ""
+                    })
+
+                processed_ips6[protein_id][gene3d_sig_acc]["locations"].append(gene3d_location)
+
+    return processed_ips6
+
+
+def main():
+    parser = build_parser()
+    args = parser.parse_args()
+
+    matches = parse_cath(args.cath_out)
+    processed_ips6 = filter_matches(args.ips6, matches)
+    print(json.dumps(processed_ips6, indent=2))
+
+
+if __name__ == "__main__":
+    main()
