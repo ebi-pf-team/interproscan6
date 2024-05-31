82b3f0c153981e411ae94d8c8b5cfdd3a3a74531
laisecf <laisecflorentino@gmail.com>
2024-05-31 13:29:39 +0100

    integration test to gene3d and funfam

--- tests/tests_integration/test_json_integrated.py
+++ tests/tests_integration/test_json_integrated.py
@@ -5,7 +5,7 @@ import subprocess
 def get_current_output(current_output_path: str, input_path: str, applications: str, disable_precalc: bool) -> dict:
     disable_precalc = "--disable_precalc" if disable_precalc else ""
     command = f"nextflow run interproscan.nf --input {input_path} --applications {applications} {disable_precalc} --formats json,tsv-pro --output {current_output_path} --goterms --pathways"
-    subprocess.run(command, shell=True)
+    # subprocess.run(command, shell=True)
     with open(str(current_output_path) + ".json", 'r') as f:
         return json.load(f)
 
@@ -22,7 +22,12 @@ def json2dict(obj):
             result[key] = json2dict(value)
         return dict(sorted(result.items()))
     elif isinstance(obj, list):
-        return sorted([json2dict(item) for item in obj], key=lambda x: str(x))
+        # Ordena todos os itens da lista recursivamente
+        sorted_list = sorted([json2dict(item) for item in obj], key=lambda x: json.dumps(x))
+        # Verifica se algum item tem a chave 'md5' e reordena com base nela se necessário
+        if sorted_list and isinstance(sorted_list[0], dict) and 'md5' in sorted_list[0]:
+            sorted_list = sorted(sorted_list, key=lambda x: x.get('md5', ''))
+        return sorted_list
     else:
         return obj
 
@@ -56,10 +61,15 @@ def test_json_output(input_path, expected_output_path, current_output_path, appl
     expected = json2dict(expected_output)
     current = json2dict(current_output)
 
-    ignore_elements = ['representative', 'location-fragments']
+    ignore_elements = ['representative']
     print("Missing elements in current output:")
     compare(expected, current, ignore_elements)
     print("Extra elements in current output:")
     compare(current, expected, ignore_elements)
 
-    # assert expected == current  # Uncomment this line when output totally implemented
+    with open('temp_expected.json', 'w') as file:
+        json.dump(expected, file, indent=2)
+    with open('temp_current.json', 'w') as file:
+        json.dump(current, file, indent=2)
+
+    assert expected == current  # Uncomment this line when output totally implemented
