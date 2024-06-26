cd3e63723ec6754109a4f4b3b4b229acc9219e40
laisecf <laisecflorentino@gmail.com>
2024-06-26 14:48:51 +0100

    updating integration tests as on main

--- tests/tests_integration/test_json_integrated.py
+++ tests/tests_integration/test_json_integrated.py
@@ -1,10 +1,14 @@
+import os
 import json
 import subprocess
 
 
 def get_current_output(current_output_path: str, input_path: str, applications: str, disable_precalc: bool) -> dict:
     disable_precalc = "--disable_precalc" if disable_precalc else ""
-    command = f"nextflow run interproscan.nf --input {input_path} --applications {applications} {disable_precalc} --formats json,tsv-pro --output {current_output_path} --goterms --pathways"
+    command = f"nextflow run interproscan.nf --input {input_path} --applications {applications} {disable_precalc} " \
+              f"--formats json,tsv-pro --output {current_output_path} --goterms --pathways"
+    # if os.path.exists(str(current_output_path) + ".json"):
+    #     os.remove(str(current_output_path) + ".json")
     # subprocess.run(command, shell=True)
     with open(str(current_output_path) + ".json", 'r') as f:
         return json.load(f)
@@ -22,36 +26,46 @@ def json2dict(obj):
             result[key] = json2dict(value)
         return dict(sorted(result.items()))
     elif isinstance(obj, list):
-        # Ordena todos os itens da lista recursivamente
         sorted_list = sorted([json2dict(item) for item in obj], key=lambda x: json.dumps(x))
-        # Verifica se algum item tem a chave 'md5' e reordena com base nela se necessário
         if sorted_list and isinstance(sorted_list[0], dict) and 'md5' in sorted_list[0]:
             sorted_list = sorted(sorted_list, key=lambda x: x.get('md5', ''))
+        if sorted_list and isinstance(sorted_list[0], dict) and 'model-ac' in sorted_list[0]:
+            sorted_list = sorted(sorted_list, key=lambda x: x.get('model-ac', ''))
         return sorted_list
     else:
         return obj
 
 
-def compare(expected, current, ignore_elements: list):
+def compare(expected, current, ignore_elements: list, print_md5: bool, print_acc: bool):
     for key in expected:
+        if key == "md5" and print_md5:
+            print(f"md5: {expected[key]}")
+            print_md5 = False
+        if key == "accession" and print_acc:
+            print(f"accession: {expected[key]}")
+            print_acc = False
         if key in ignore_elements:
             continue
         if key not in current:
-            print(f"Key '{key}' missing in current dict")
+            print(f"MISMATCH: Key '{key}' missing in current dict")
+            print_md5 = True
+            print_acc = True
             continue
         if isinstance(expected[key], dict):
-            compare(expected[key], current[key], ignore_elements)
+            compare(expected[key], current[key], ignore_elements, print_md5, print_acc)
         elif isinstance(expected[key], list):
             if len(expected[key]) != len(current[key]):
-                print(f"List length mismatch for key '{key}'")
+                print(f"MISMATCH: list length for key '{key}'")
             else:
                 for i in range(len(expected[key])):
-                    compare(expected[key][i], current[key][i], ignore_elements)
+                    compare(expected[key][i], current[key][i], ignore_elements, print_md5, print_acc)
         else:
             if str(expected[key]).lower() != str(current[key]).lower():
-                print(f"Value mismatch for key '{key}'")
+                print(f"MISMATCH: for key '{key}'")
                 print(f"  expected: {expected[key]}")
                 print(f"  current: {current[key]}")
+                print_md5 = True
+                print_acc = True
 
 
 def test_json_output(input_path, expected_output_path, current_output_path, applications, disable_precalc):
@@ -61,15 +75,13 @@ def test_json_output(input_path, expected_output_path, current_output_path, appl
     expected = json2dict(expected_output)
     current = json2dict(current_output)
 
-    ignore_elements = ['representative']
-    print("Missing elements in current output:")
-    compare(expected, current, ignore_elements)
-    print("Extra elements in current output:")
-    compare(current, expected, ignore_elements)
-
-    with open('temp_expected.json', 'w') as file:
+    with open('/Users/lcf/PycharmProjects/interproscan6/tests/integration_tests/temp_expected.json', 'w') as file:
         json.dump(expected, file, indent=2)
-    with open('temp_current.json', 'w') as file:
+    with open('/Users/lcf/PycharmProjects/interproscan6/tests/integration_tests/temp_current.json', 'w') as file:
         json.dump(current, file, indent=2)
 
-    assert expected == current  # Uncomment this line when output totally implemented
+    ignore_elements = ['representative', 'evalue']
+    compare(expected, current, ignore_elements, True, True)
+    # compare(current, expected, ignore_elements, False, False)
+
+    assert expected == current
