376457a486f632925915ced0d19414d1b82de242
HobnobMancer <emma.e.hobbs@outlook.com>
2024-07-08 09:25:33 +0100

    set integration tests to test gene3d and funfam

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -27,7 +27,7 @@ def input_path(test_input_dir):
 
 @pytest.fixture
 def expected_output_path(test_output_dir):
-    return test_output_dir / "expected_output_hamap"
+    return test_output_dir / "expected_output_gene3d_funfam"
 
 
 @pytest.fixture
@@ -36,13 +36,8 @@ def current_output_path(test_output_dir):
 
 
 @pytest.fixture
-def expected_output_path(test_output_dir):
-    return test_output_dir / "expected_output_pfam_fragm"
-
-
-@pytest.fixture
 def applications():
-    return "pfam"
+    return "gene3d,funfam"
 
 
 @pytest.fixture
