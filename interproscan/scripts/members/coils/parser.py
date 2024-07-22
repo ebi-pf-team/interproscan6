44c51b12be9edeee43a73bdbe3b21d988b933d3d
Ailsa Orr <ako@ebi.ac.uk>
2024-07-22 10:42:01 +0100

    changed coil plural name

--- interproscan/scripts/members/coils/parser.py
+++ interproscan/scripts/members/coils/parser.py
@@ -14,7 +14,7 @@ def parse(coils_out: str, version: float) -> dict:
         for line in coils_file:
             if line.startswith(">"):
                 seq_id = line.strip(">").strip("\n").split()[0]
-                match = {"Coils": {"member_db": "Coils", "version": version, "name": "Coils", "accession":"Coils", "locations": []}}
+                match = {"Coil": {"member_db": "Coils", "version": version, "name": "Coil", "accession":"Coil", "locations": []}}
                 matches[seq_id] = match
             elif not line.startswith((">", "//", "\n")):
                 line = line.split()
@@ -23,7 +23,7 @@ def parse(coils_out: str, version: float) -> dict:
                     location_end = line[1]
                     location_fragments = {"start": int(location_start), "end": int(location_end), "dc-status": "CONTINUOUS"}
                     location = {"start": location_start, "end": location_end, "representative": "false", "location-fragments": [location_fragments]}
-                    matches[seq_id]["Coils"]["locations"].append(location)
+                    matches[seq_id]["Coil"]["locations"].append(location)
     return matches
 
 
