39051c61af3375c7ea5f2d3958679ff0273c4378
Ailsa Orr <ako@ebi.ac.uk>
2024-07-22 10:21:18 +0100

    fixed missing matches

--- interproscan/scripts/members/coils/parser.py
+++ interproscan/scripts/members/coils/parser.py
@@ -13,20 +13,16 @@ def parse(coils_out: str, version: float) -> dict:
     with open(coils_out) as coils_file:
         for line in coils_file:
             if line.startswith(">"):
-                seq_id = line[1:].strip(">").strip("\n")
+                seq_id = line.strip(">").strip("\n").split()[0]
                 match = {"Coils": {"member_db": "Coils", "version": version, "name": "Coils", "accession":"Coils", "locations": []}}
                 matches[seq_id] = match
-
             elif not line.startswith((">", "//", "\n")):
                 line = line.split()
                 if line:
                     location_start = line[0]
                     location_end = line[1]
-                    location = {"start": location_start, "end": location_end, "representative": "false", "location-fragments": [
-                    {"start": int(location_start),
-                     "end": int(location_end),
-                     "dc-status": "CONTINUOUS"}
-                ]}
+                    location_fragments = {"start": int(location_start), "end": int(location_end), "dc-status": "CONTINUOUS"}
+                    location = {"start": location_start, "end": location_end, "representative": "false", "location-fragments": [location_fragments]}
                     matches[seq_id]["Coils"]["locations"].append(location)
     return matches
 
