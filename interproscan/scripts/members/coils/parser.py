b2c62200357a906ccdac832a5ff06dfd50b617c3
Ailsa Orr <ako@ebi.ac.uk>
2024-07-23 10:50:42 +0100

    formatting

--- interproscan/scripts/members/coils/parser.py
+++ interproscan/scripts/members/coils/parser.py
@@ -14,15 +14,22 @@ def parse(coils_out: str, version: float) -> dict:
         for line in coils_file:
             if line.startswith(">"):
                 seq_id = line.strip(">").strip("\n").split()[0]
-                match = {"Coil": {"member_db": "Coils", "version": version, "name": "Coil", "accession":"Coil", "locations": []}}
+                match = {"Coil": {"member_db": "Coils",
+                                  "version": version, "name": "Coil",
+                                  "accession":"Coil", "locations": []}}
                 matches[seq_id] = match
-            elif not line.startswith((">", "//", "\n")):
+            else:
                 line = line.split()
-                if line:
+                if len(line) > 1:
                     location_start = line[0]
                     location_end = line[1]
-                    location_fragments = {"start": int(location_start), "end": int(location_end), "dc-status": "CONTINUOUS"}
-                    location = {"start": location_start, "end": location_end, "representative": "false", "location-fragments": [location_fragments]}
+                    location_fragments = {"start": int(location_start),
+                                          "end": int(location_end),
+                                          "dc-status": "CONTINUOUS"}
+                    location = {"start": location_start,
+                                "end": location_end,
+                                "representative": "false",
+                                "location-fragments": [location_fragments]}
                     matches[seq_id]["Coil"]["locations"].append(location)
     return matches
 
