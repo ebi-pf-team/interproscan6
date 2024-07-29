3442bcc1d4ce10282fc243d42994f30c48ffbe75
Ailsa Orr <ako@ebi.ac.uk>
2024-07-29 15:57:35 +0100

    cleaned output

--- interproscan/scripts/output/format_writer/tsv_output.py
+++ interproscan/scripts/output/format_writer/tsv_output.py
@@ -35,8 +35,6 @@ def tsv_output(seq_matches: dict, output_path: str):
                         goterms.append(go_info["id"])
                     for pwy_info in match["entry"]["pathwayXRefs"]:
                         pathways.append(pwy_info["id"])
-                if match["member_db"].upper() == "COILS":
-                    entry_desc = "Coil"
                 match_db = match["member_db"]
                 xrefs = f"{'|'.join(goterms)}\t{'|'.join(pathways)}"
 
