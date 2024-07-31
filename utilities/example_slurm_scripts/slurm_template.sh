4a4edcad4efc429ef95df0eef38a799fe342ef5f
HobnobMancer <emma.e.hobbs@outlook.com>
2024-07-31 08:17:13 +0100

    add template slurm script

---
+++ utilities/example_slurm_scripts/slurm_template.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=ips6-test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=30:00
+#SBATCH --mem=20G
+
+cd $SLURM_SUBMIT_DIR
+
+# add code to load container runtime and nextflow software
+
+nextflow run interproscan.nf \
+    -profile slurm,singularity \
+    --input utilities/test_files/best_to_test.fasta \
+    --disable_precalc \
+    -work-dir /lscratch \
+    --applications antifam,ncbifam,pfam
