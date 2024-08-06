8c60824acc7c46dd7c4d2783dc66fc827bad216e
HobnobMancer <emma.e.hobbs@outlook.com>
2024-08-06 10:47:05 +0100

    add nucleic seqs to the parsed_seqs temp dict

--- interproscan/scripts/parse_sequence/parse_sequence.py
+++ interproscan/scripts/parse_sequence/parse_sequence.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
+import re
 import sys
+
+
+NT_SEQ_ID_PATTERN = re.compile(r"^orf\d+\s+source=(.*)\s+coords=(\d+\.+\d+)\s+.+frame=\d+\s+desc=.*$")
 
 
 def is_fasta_check(fasta_file: str):
@@ -10,14 +14,15 @@ def is_fasta_check(fasta_file: str):
             raise ValueError(f"{fasta_file} is not in FASTA format")
 
 
-def get_sequences(fasta_file: str) -> dict:
+def get_sequences(fasta_file: str, nucleic=False) -> dict:
     sequences = {}
     with open(fasta_file, "r") as f:
         lines = f.readlines()
         for line in lines:
             line = line.strip()
             if line.startswith(">"):
-                seq_key = line[1:]
+                # split on the first white space to match GETORF handling of seqIDs
+                seq_key = line[1:] if not nucleic else line[1:].split(maxsplit=1)[0]
                 sequences[seq_key] = ""
             else:
                 sequences[seq_key] += line
@@ -37,11 +42,38 @@ def parse(sequences: dict):
     return results
 
 
+def parse_nucleic(sequences: dict, nt_seqs: dict):
+    results = {}
+    for key, sequence in sequences.items():
+        sequence_info = []
+        acc = key.split(" ", 1)[0]
+        sequence_info.append(key)
+        sequence_info.append(sequence)
+        sequence_info.append(hashlib.md5(sequence.encode()).hexdigest())
+        sequence_info.append(len(sequence))
+        sequence_info.append(nt_seqs[NT_SEQ_ID_PATTERN.match(key).group(1)])
+        results[acc] = sequence_info
+    return results
+
+
 def main():
+    """
+    args[0] = str repr of path to FASTA file of query protein sequences
+        (may be translated seqs from predicted ORF)
+    args[1] = str repr of path to originally submitted FASTA file
+        (may contain the original nucleic sequences)
+    args[2] = str repr of bool, if nucleic seqs provided ('true') or not ('false')"""
     args = sys.argv[1:]
     is_fasta_check(args[0])
     sequences = get_sequences(args[0])
-    sequence_parsed = parse(sequences)
+
+    if args[2] == "true":
+        is_fasta_check(args[1])
+        nt_seqs = get_sequences(args[1], nucleic=True)
+        sequence_parsed = parse_nucleic(sequences, nt_seqs)
+    else:
+        sequence_parsed = parse(sequences)
+
     print(json.dumps(sequence_parsed))
 
 
