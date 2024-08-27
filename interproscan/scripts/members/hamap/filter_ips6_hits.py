58b05a6dc2405900cbe3916259932db6b574fd5e
HobnobMancer <emma.e.hobbs@outlook.com>
2024-08-27 10:17:17 +0100

    remove arparsers from hamap filter script

--- interproscan/scripts/members/hamap/filter_ips6_hits.py
+++ interproscan/scripts/members/hamap/filter_ips6_hits.py
@@ -1,5 +1,5 @@
-import argparse
 import json
+import sys
 
 from pathlib import Path
 
@@ -44,38 +44,6 @@ class DomainHit:
         self.symbol = None  # do not know this represents, often '+'
         self.seq = None
         self.match_id = None
-
-
-def build_parser() -> argparse.ArgumentParser:
-    """Build cmd-line argument parser"""
-    parser = argparse.ArgumentParser(
-        prog="hamap_match_parser",
-        description=(
-            "Parse the output from the pfsearch_wrapper.py "
-            "into the interal IPS6 JSON structure"
-        ),
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-
-    parser.add_argument(
-        "ips6",
-        type=Path,
-        help="Path to an internal IPS6 JSON file"
-    )
-
-    parser.add_argument(
-        "pf_output",
-        type=Path,
-        help="Path to pfsearch_wrapper.y output file"
-    )
-
-    parser.add_argument(
-        "out_json",
-        type=Path,
-        help="Path to write the output JSON file"
-    )
-
-    return parser
 
 
 def parse_pf_out(pf_output: Path) -> dict[str, HamapHit]:
@@ -157,13 +125,20 @@ def filter_matches(ips6: Path, hamap_matches: dict[str, HamapHit]) -> tuple[dict
 
 
 def main():
-    parser = build_parser()
-    args = parser.parse_args()
+    """CL input:
+    0. Internal IPS6 JSON
+    1. pfsearch_wrapper.py output file
+    2. Str repr of path for final output JSON
+    """
+    args = sys.argv[1:]
+    ips6_json = args[0]
+    pf_output = args[1]
+    output = args[2]
 
-    matches = parse_pf_out(args.pf_output)
-    processed_ips6 = filter_matches(args.ips6, matches)
+    matches = parse_pf_out(pf_output)
+    processed_ips6 = filter_matches(ips6_json, matches)
 
-    with open(args.out_json, "w") as fh:
+    with open(output, "w") as fh:
         json.dump(processed_ips6, fh)
 
 
