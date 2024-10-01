582cbcd84059f5615e4d6b423c60034a12c9ba6b
Matthias Blum <mat.blum@gmail.com>
2024-10-01 16:29:56 +0100

    Groovy class for param validation

---
+++ lib/InterProScan.groovy
@@ -0,0 +1,140 @@
+class InterProScan {
+    static final def PARAMS = [
+        [
+            name: "input",
+            required: true,
+            metavar: "<FASTA>",
+            description: "path to FASTA file of sequences to be analysed."
+        ],
+        [
+            name: "datadir",
+            required: true,
+            metavar: "<DATA-DIR>",
+            description: "path to data directory."
+        ],
+        [
+            name: "applications",
+            metavar: "<APPLICATIONS>",
+            description: "comma-separated applications to scan the sequences with. Default: all."
+        ],
+        [
+            name: "formats",
+            metavar: "<FORMATS>",
+            description: "comma-separated output formats. Available: JSON,TSV,XML. Default: JSON,TSV,XML."
+        ],
+        [
+            name: "outdir",
+            metavar: "<OUTDIR>",
+            description: "output directory where results will be saved. Default: current working directory."
+        ],
+        [
+            name: "disable-precalc",
+            description: "treat input sequences as DNA/RNA."
+        ],
+        [
+            name: "precalc-url",
+            metavar: "<URL>",
+            description: "URL of the match lookup service. Default: https://www.ebi.ac.uk/interpro/match-lookup."
+        ],
+        [
+            name: "nucleic",
+            description: "do no retrieve pre-calculated matches from the match lookup service."
+        ],
+        [
+            name: "go-terms",
+            description: "include Gene Ontology (GO) mapping in output files."
+        ],
+        [
+            name: "pathways",
+            description: "include pathway mapping in output files."
+        ],
+        [
+            name: "help",
+            description: "print the help message and exit."
+        ],
+        // No description -> not displayed in the help message
+        [
+            name: "batch-size",
+            description: null
+        ],
+        [
+            name: "signalp-mode",
+            description: null
+        ],
+        [
+            name: "signalp-gpu",
+            description: null
+        ],
+
+    ]
+
+    static void checkParams(params) {
+        def allowedParams = this.PARAMS.collect { it.name.toLowerCase() }
+
+        for (e in params) {
+            def paramName = e.key
+            def paramValue = e.value
+
+            if (paramName.contains("-")) {
+                /*
+                    From https://www.nextflow.io/docs/latest/cli.html#pipeline-parameters
+                    When the parameter name is formatted using `camelCase`, 
+                    a second parameter is created with the same 
+                    value using kebab-case, and vice versa.
+
+                    However, we don't want to evalue the `kebab-case` params.
+                    And they will eventually be ignored by NF directly,
+                    see https://github.com/nextflow-io/nextflow/pull/4702.
+                */
+                continue
+            } 
+
+            // Convert to kebab-case
+            def kebabParamName = this.camelToKebab(paramName)
+            if (!allowedParams.contains(kebabParamName.toLowerCase())) {
+                println "Unrecognized option: --${kebabParamName}."
+                println "Try '--help' for more information."
+                System.exit(1)
+            }
+
+            
+        }
+    }
+
+    static String kebabToCamel(String kebabName) {
+        return kebabName.split('-').toList().indexed().collect { index, word ->
+            index == 0 ? word : word.capitalize()
+        }.join('')
+    }
+
+    static String camelToKebab(String camelName) {
+        return camelName.replaceAll(/([a-z])([A-Z])/, '$1-$2').toLowerCase()
+    }
+
+    public static void printHelp(manifest) {
+        def result = new StringBuilder()
+        result << "Usage: nextflow run ebi-pf-team/interproscan6 -profile <PROFILE> --input <FASTA> --datadir <DATADIR> \n\n"
+        result << "Mandatory parameters:\n"
+        result << "  -profile <PROFILE>: use this parameter to choose a configuration profile.\n"
+
+        this.PARAMS.findAll{ it.required }.each { param ->
+            result << this.formatOption(param) << "\n"
+        }
+
+        result << "\nOptional parameters:\n"
+        this.PARAMS.findAll{ !it.required && it.description }.each { param ->
+            result << this.formatOption(param) << "\n"
+        }
+
+        print result.toString()
+    }
+
+    static String formatOption(option) {
+        def text = "  --${option.name}"
+        if (option.metavar) {
+            text += " ${option.metavar}"
+        }
+
+        return text.padRight(40) + ": ${option.description}"
+    }
+}
