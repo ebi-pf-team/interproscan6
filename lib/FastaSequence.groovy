75dd67ba992780669ae7fa38bb35448cec5a2d8b
Matthias Blum <mat.blum@gmail.com>
2024-10-02 22:28:25 +0100

    Remove method moved to separate class

--- lib/FastaSequence.groovy
+++ lib/FastaSequence.groovy
@@ -1,6 +1,6 @@
 import java.security.MessageDigest
 
-class FastaSequence {
+class FastaSequence implements Serializable {
     // DNA, RNA, gaps
     static final String NUCLEIC_ALPHABET = ~/(?i)[ACGTUN\-\.\s]+/
     // 20 standard AAs, Sec, Pyl, any/unknown, Asx, Glx, Xle, gaps
@@ -9,11 +9,12 @@ class FastaSequence {
     String id
     String description
     String sequence
+    String md5 = null
 
-    String getMD5() {
+    void setMD5() {
         MessageDigest md = MessageDigest.getInstance("MD5")
-        byte[] digest = md.digest(sequence.bytes)
-        return digest.encodeHex().toString()
+        byte[] digest = md.digest(sequence.toUpperCase().bytes)
+        this.md5 = digest.encodeHex().toString().toUpperCase()
     }
 
     String validate(boolean isNucleic = false, String extraForbiddenChars = "") {
@@ -33,48 +34,5 @@ class FastaSequence {
         }
         
         return null
-    }
-
-    static void checkFastaFile(String fastaFilePath, boolean isNucleic, Map appsConfig, List<String> appsToRun) {
-        // Add application-specific forbidden characters
-        String forbiddenChars = ""
-        appsToRun.each{ app ->
-            forbiddenChars += appsConfig[app].invalidChars ?: ""
-        }
-
-        // Remove duplicate characters
-        forbiddenChars = forbiddenChars.toSet().join("")
-        
-        // Parse fasta file and check each sequence
-        String errors = null
-        FastaSequence currentSequence = null
-        new File(fastaFilePath).eachLine { line ->
-            if (line.startsWith(">")) {
-                if (currentSequence) {
-                    errors = currentSequence.validate(isNucleic, forbiddenChars)
-                    if (errors) {
-                        println "Error: forbidden characters found in sequence ${currentSequence.id}: ${errors}"
-                        System.exit(1)
-                    }
-                }
-
-                def header = line.substring(1).split(" ", 2).collect { it.trim() }
-                currentSequence = new FastaSequence(
-                    id: header[0], 
-                    description: header.size() > 1 ? header[1] : "", 
-                    sequence: ""
-                )
-            } else {
-                currentSequence.sequence += line.trim()
-            }
-        }
-
-        if (currentSequence) {
-            errors = currentSequence.validate(isNucleic, forbiddenChars)
-            if (errors) {
-                println "Error: forbidden characters found in sequence ${currentSequence.id}: ${errors}"
-                System.exit(1)
-            }
-        }
     }
 }
