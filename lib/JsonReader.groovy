dda76cf36ab859b61a34b6a9ad78f88c99a55adb
HobnobMancer <emma.e.hobbs@outlook.com>
2025-01-31 11:07:51 +0000

    add method for loading entire json file as jsonNodes

--- lib/JsonReader.groovy
+++ lib/JsonReader.groovy
@@ -8,6 +8,16 @@ import com.fasterxml.jackson.databind.ObjectMapper
 
 class JsonReader {
     static stream(String filePath, ObjectMapper mapper, Closure closure) {
+        /*
+        Stream the data, one jsonNode at a time from a Json file containing a JsonArray.
+        It requires less memory to store maps as JsonNodes than as Maps,
+        especially when the maps contain nested maps.
+        To use:
+        ObjectMapper mapper = new ObjectMapper().enabled(SerializationFeature.INDENT_OUTPUT)
+        JsonReader.stream(filePath, mapper) { JsonNode node ->
+            md5 = node.get("md5").asText()
+        }
+         */
         try {
             JsonFactory factory = mapper.getFactory()
             JsonParser parser = factory.createParser(new File(filePath))
@@ -19,13 +29,46 @@ class JsonReader {
             }
             parser.close()
         } catch (FileNotFoundException e) {
-            throw new Exception("File not found: $jsonFile", e)
+            throw new Exception("File not found: $filePath", e)
         } catch (JsonParseException e) {
-            throw new Exception("Error parsing JSON file: $jsonFile", e)
+            throw new Exception("Error parsing JSON file: $filePath", e)
         } catch (JsonMappingException e) {
-            throw new Exception("Error mapping JSON content for file: $jsonFile", e)
+            throw new Exception("Error mapping JSON content for file: $filePath", e)
         } catch (IOException e) {
-            throw new Exception("IO error reading file: $jsonFile", e)
+            throw new Exception("IO error reading file: $filePath", e)
+        }
+    }
+
+    static Map<String, JsonNode> load(String filePath, ObjectMapper mapper) {
+        /*
+        Load all contents of a json file into the memory, storing internal nodes as JsonNodes
+        to reduce memory requirements compared to storing the data in memory as maps.
+        To use:
+        ObjectMapper mapper = new ObjectMapper().enabled(SerializationFeature.INDENT_OUTPUT)
+        def matchesMap = JsonReader.load(filePath, mapper)
+        seqMatches = matchesMap.get("seqId_1")
+        */
+        try {
+            File file = new File(filePath)
+            JsonNode rootNode = mapper.readTree(file)
+            if (!rootNode.isObject()) {
+                throw new JsonMappingException("Expected JSON object at root level")
+            }
+
+            Map<String, JsonNode> dataMap = new HashMap<>()
+            rootNode.fields().each { entry ->
+                dataMap.put(entry.key, entry.value)
+            }
+
+            return dataMap
+        } catch (FileNotFoundException e) {
+            throw new Exception("File not found: $filePath", e)
+        } catch (JsonParseException e) {
+            throw new Exception("Error parsing JSON file: $filePath", e)
+        } catch (JsonMappingException e) {
+            throw new Exception("Error mapping JSON content for file: $filePath", e)
+        } catch (IOException e) {
+            throw new Exception("IO error reading file: $filePath", e)
         }
     }
 }
