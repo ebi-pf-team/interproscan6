b4bf3f9a6757bc3eff290afba6e9edd8ae3e8c08
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 <laisecflorentino@gmail.com>
2025-04-10 09:43:48 +0100

    using json i6 and bymember opt

--- tests/regression_tests/test_swissprot_gain_loss.py
+++ tests/regression_tests/test_swissprot_gain_loss.py
@@ -1,135 +1,127 @@
 import os
 import re
 import argparse
+import json
 
 import oracledb
 
 
 # Members integrated on InterPro
-MEMBER2CODE = {
-    "X": "CATH-Gene3D",
-    "J": "CDD",
-    "Q": "HAMAP",
-    "N": "NCBIFAM",
-    "V": "PANTHER",
-    "H": "Pfam",
-    "U": "PIRSF",
-    "F": "PRINTS",
-    "P": "PROSITE patterns",
-    "M": "PROSITE profiles",
-    "B": "SFLD",
-    "R": "SMART",
-    "Y": "SUPERFAMILY"
+PREFIX2MEMBERDB = {
+    "G3DSA": "CATH-Gene3D",
+    "cd": "CDD",
+    "MF": "HAMAP",
+    "NF": "NCBIFAM",
+    "TIGR": "NCBIFAM (TIGRFAM)",
+    "PTHR": "PANTHER",
+    "PF": "Pfam",
+    "PIRSF": "PIRSF",
+    "PR": "PRINTS",
+    "PS": "PROSITE",
+    "SFLD": "SFLD",
+    "SM": "SMART",
+    "SSF": "SUPERFAMILY"
 }
 
 
-def parse_i6_tsv(i6_output: str):
-    interpro2acc = {}
-    for member in MEMBER2CODE.values():
-        interpro2acc[member] = {}
-
-    with open(i6_output, "rt") as fh:
-        lines = fh.readlines()
-
-    for line in lines:
-        values = line.split("\t")
-        interpro_acc = values[11]
-        member_acc = values[4]
-        member_db = values[3]
-        # Ignore PANTHER subfamilies (won't be integrated)
-        if re.fullmatch(r"PTHR\d+:SF\d+", member_acc):
-            continue
-
-        try:
-            interpro2acc[member_db][interpro_acc].add(member_acc)
-        except KeyError:
-            interpro2acc[member_db][interpro_acc] = {member_acc}
-    return interpro2acc
-
-
-def swissprot_comparison_i5_i6(ora_url: str, i6_output: str):
-    # Get Swiss-Prot <-> Interpro mapping (i6)
-    sig2swiss_i6 = parse_i6_tsv(i6_output)
+def get_i5_swissprot_mapping(ora_url: str):
+    entry2sigs_i5 = {}
 
     con = oracledb.connect(ora_url)
     cur = con.cursor()
-    info = {}
+
     cur.execute(
         """
-        SELECT M.METHOD_AC, M.DBCODE, E.ENTRY_AC, E.ENTRY_TYPE, E.NAME
+        SELECT E.ENTRY_AC, M.METHOD_AC
         FROM INTERPRO.MATCH M
         INNER JOIN INTERPRO.ENTRY2METHOD EM ON M.METHOD_AC = EM.METHOD_AC
         INNER JOIN INTERPRO.ENTRY E ON EM.ENTRY_AC = E.ENTRY_AC
         INNER JOIN INTERPRO.PROTEIN P ON M.PROTEIN_AC = P.PROTEIN_AC
         WHERE E.CHECKED = 'Y' AND P.DBCODE = 'S'
-         and M.DBCODE = 'N'
         """
     )
 
-    sig2swiss_i5 = {}
-    for member in MEMBER2CODE.values():
-        sig2swiss_i5[member] = {}
-
-    for row in cur.fetchall():
-        member_db = MEMBER2CODE[row[1]]
-        interpro_acc = row[2]
-        member_acc = row[0]
-        type = row[3]
-        desc = row[4]
-        # 'PF06777': ('H', 'IPR010643', 'D', 'Helical and beta-bridge domain')
-        info[member_acc] = (member_db, interpro_acc, type, desc)
+    for entry, signature in cur.fetchall():
         try:
-            sig2swiss_i5[member_db][row[2]].add(row[0])
+            entry2sigs_i5[entry].add(signature)
         except KeyError:
-            sig2swiss_i5[member_db][row[2]] = {row[0]}
-
-    for database in sig2swiss_i5:
-        # Compare signatures
-        count_lost = 0
-        count_gained = 0
-        dict_lost = {}
-        dict_gained = {}
-
-        for interpro_acc in sig2swiss_i5[database]:
-            # Get all members of the signature
-            members_i5 = sig2swiss_i5[database][interpro_acc]
-            members_i6 = sig2swiss_i6[database].get(interpro_acc, set())
-
-            if members_i5 != members_i6:
-                lost = members_i5 - members_i6
-                gained = members_i6 - members_i5
-                if lost:
-                    count_lost += len(lost)
-                    try:
-                        dict_lost[interpro_acc].update(lost)
-                    except KeyError:
-                        dict_lost[interpro_acc] = lost
-                if gained:
-                    count_gained += len(gained)
-                    try:
-                        dict_gained[interpro_acc].update(lost)
-                    except KeyError:
-                        dict_gained[interpro_acc] = lost
-        print(f"{database}:")
-        print(f"  #Lost: {count_lost}")
-        if count_lost > 0:
-            print(f"    Lost: {dict_lost}")
-        print(f"  #Gained: {count_gained}")
-        if count_gained > 0:
-            print(f"    Gained: {dict_gained}")
+            entry2sigs_i5[entry] = {signature}
 
     cur.close()
     con.close()
 
+    return entry2sigs_i5
+
+
+def get_i6_swissprot_mapping(i6_result: dict):
+    entry2sigs = {}
+
+    for protein_dict in i6_result["results"]:
+        for match in protein_dict["matches"]:
+            try:
+                entry = match["signature"]["entry"]
+                if not entry:
+                    continue
+                else:
+                    entry_acc = match["signature"]["entry"]["accession"]
+            except TypeError:
+                print(f"KeyError: {match['signature']}")
+            signature = match["signature"]["accession"]
+
+            # Ignore PANTHER subfamilies (won't be integrated)
+            if re.fullmatch(r"PTHR\d+:SF\d+", signature):
+                continue
+
+            try:
+                entry2sigs[entry_acc].add(signature)
+            except KeyError:
+                entry2sigs[entry_acc] = {signature}
+
+    return entry2sigs
+
+
+def swissprot_comparison(entry2sigs_i5: dict, entry2sigs_i6: dict):
+    count_total_lost = 0
+    count_total_gained = 0
+    for entry in entry2sigs_i5:
+        members_i6 = entry2sigs_i6.get(entry, set())
+        members_i5 = entry2sigs_i5.get(entry, set())
+        if members_i5 != members_i6:
+            lost = members_i5 - members_i6
+            gained = members_i6 - members_i5
+            if lost or gained:
+                count_total_lost += len(lost)
+                count_total_gained += len(gained)
+                print(f"Entry: {entry} | #Lost: {len(lost)} | #Gained: {len(gained)} | Lost: {lost} | Gained: {gained}")
+    print(f"Total lost: {count_total_lost} | Total gained: {count_total_gained}")
+
 
 def main():
-    parser = argparse.ArgumentParser(prog="IPS_match_regression_test", description="Check presence of matches", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--i6output", type=str, default="tests/data/uniprot_sprot.fasta.tsv", help="TSV with i6 SwissProt result")
+    parser = argparse.ArgumentParser(prog="IPS_swissprot_test", description="Check gain/lost of swissprot between i5 and i6", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--i6output", type=str, help="i6 SwissProt JSON result")
+    parser.add_argument("--bymembers", help="shows gain/lost by memberDB", action="store_true")
 
     args = parser.parse_args()
-
     ora_url = os.environ["ORACLE_URL"]
-    swissprot_comparison_i5_i6(ora_url, args.i6output)
+
+    with open(args.i6output, "r") as fh:
+        entry2sigs_i6 = get_i6_swissprot_mapping(json.load(fh))
+
+    entry2sigs_i5 = get_i5_swissprot_mapping(ora_url)
+
+    if args.bymembers:
+        # Compare by memberDB
+        for prefix in PREFIX2MEMBERDB:
+            entry2sig_i5_by_member = {}
+            entry2sig_i6_by_member = {}
+            for entry, signatures in entry2sigs_i5.items():
+                entry2sig_i5_by_member[entry] = {signature for signature in signatures if signature.startswith(prefix)}
+            for entry, signatures in entry2sigs_i6.items():
+                entry2sig_i6_by_member[entry] = {signature for signature in signatures if signature.startswith(prefix)}
+            print(f"{PREFIX2MEMBERDB[prefix]}:")
+            swissprot_comparison(entry2sig_i5_by_member, entry2sig_i6_by_member)
+    else:
+        swissprot_comparison(entry2sigs_i5, entry2sigs_i6)
 
 
 if __name__ == '__main__':
