12c0ddd02d8ef56269f58c0fd49258bb926e2e26
Matthias Blum <mat.blum@gmail.com>
2025-10-18 00:51:20 +0100

    Move some classes to their own module

---
+++ lib/Signature.groovy
@@ -0,0 +1,65 @@
+import Entry
+import SignatureLibraryRelease
+
+class Signature implements Serializable {
+    // The order of the fields here determines their order in the final output files
+    String accession
+    String name
+    String description
+    String type
+    SignatureLibraryRelease signatureLibraryRelease = new SignatureLibraryRelease(null, null)
+    Entry entry = null
+
+    Signature(String accession) {
+        this.accession = accession
+    }
+
+    Signature(String accession, SignatureLibraryRelease library) {
+        this.accession = accession
+        this.signatureLibraryRelease = library
+    }
+
+    Signature(String accession,
+              String name,
+              String description,
+              SignatureLibraryRelease library,
+              Entry entry) {
+        this.accession = accession
+        this.name = name
+        this.description = description
+        this.signatureLibraryRelease = library
+        this.entry = entry
+    }
+
+    Signature(String accession,
+              String name,
+              String description,
+              String type,
+              SignatureLibraryRelease library,
+              Entry entry) {
+        this.accession = accession
+        this.name = name
+        this.description = description
+        this.type = type
+        this.signatureLibraryRelease = library
+        this.entry = entry
+    }
+
+    void setType(String type) {
+        this.type = type
+    }
+
+    static Signature fromMap(Map data) {
+        if (data == null) {
+            return null
+        }
+        return new Signature(
+                data.accession,
+                data.name,
+                data.description,
+                data.containsKey("type") ? data.type : null,  // Provide a default value (null) if 'type' is missing
+                SignatureLibraryRelease.fromMap(data.signatureLibraryRelease),
+                Entry.fromMap(data.entry)
+        )
+    }
+}
