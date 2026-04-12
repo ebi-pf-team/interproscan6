23767e4fd2772e4202da2abf917b215505e502b2
Matthias Blum <mat.blum@gmail.com>
2026-04-12 13:03:49 +0100

    Remove groovy scripts

--- docker/groovy/select-repr-locations.groovy
+++
@@ -1,9 +0,0 @@
-import java.nio.file.Paths
-import uk.ac.ebi.interpro.ProcessReprLocations
-
-if (args.size() != 2) {
-    System.err.println "Usage: select-repr-locations.groovy <input.json> <output.json>"
-    System.exit(1)
-}
-
-ProcessReprLocations.run(Paths.get(args[0]), Paths.get(args[1]))
